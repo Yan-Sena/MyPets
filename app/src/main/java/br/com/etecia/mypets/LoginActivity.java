9e88a95073b5e4f62fe2741d30eae8aca7086743
lndsilva <lndsilva@hotmail.com>
2022-03-30 20:13:16 -0300

    Aplicando material designer

--- app/src/main/java/br/com/etecia/mypets/LoginActivity.java
+++ app/src/main/java/br/com/etecia/mypets/LoginActivity.java
@@ -3,6 +3,8 @@ package br.com.etecia.mypets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+    }
+
+    public void acessarSistema(View view) {
+        Toast.makeText(getApplicationContext(),"Bem vindo ao sistema",Toast.LENGTH_LONG).show();
     }
 }
